a6120b7e4a362b369727a22c5f7f4d825b2f135d
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2023-01-28 15:45:01 -0300

    Mapping API endpints with resources tutorial step

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Admin } from "react-admin";
+import { GlobalStyle } from './styles/GlobalStyle'
+
+import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-export function App() {
-  return (
-    <>
-      <Admin dataProvider={dataProvider} />
-    </>
-  )
-}
+export const App = () => (
+  <>
+    <GlobalStyle />
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  </>
+);
