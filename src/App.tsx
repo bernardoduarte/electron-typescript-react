5293fbc8cff44910944cde6f841d27c9ee87d932
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2023-01-28 16:03:28 -0300

    Jumped a few steps and setup login and a dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { GlobalStyle } from './styles/GlobalStyle'
 
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { Dashboard } from './components/Dashboard';
+import { authProvider } from './providers/authProvider';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 export const App = () => (
   <>
     <GlobalStyle />
-    <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+    <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard} >
+      <Resource name="users" list={ListGuesser} edit={EditGuesser} />
+      <Resource name="posts" list={ListGuesser} edit={EditGuesser} />
     </Admin>
   </>
 );
